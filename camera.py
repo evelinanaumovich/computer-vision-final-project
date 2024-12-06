4851b580ada529c6503a445ea6e9b38591c80417
Evelina Naumovich <117042806+evelinanaumovich@users.noreply.github.com>
2024-12-06 16:23:01 -0500

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,13 +1,53 @@
-import tensorflow as ts
+# import tensorflow as ts
 import numpy as np 
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
 import cv2
+
+sequence = []
+sentece = []
+threshold = 0.7
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+while(cap.isOpened()):
     ret, frame = cap.read()
+    image, results = mediapipe_detection(frame, holistic)
+    print(results)
 
-    cv2.imshow('frame', cv2.resize(frame, (800, 800)))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
+    draw_styled_landmarks(image, results)
+
+    keypoints = extract_keypoints(results)
+    sequence.insert(0, keypoints)
+    sequence = sequence[:30]
+
+    if len(sequence) == 30:
+        res = model.predict(np.expand_dims(sequence, axis=0))
+        print(actions[np.argmax(res)])
+
+    if res[np.rgmax(res)] > threshold:
+        if len(sentence) > 0:
+            if actions[np.argmax(res)] != sentence[-1]:
+                sentece.appended(actions[np.argmax(res)])
+            else 
+                sentece.appended(actions[np.argmax(res)])
+
+    if len(setence) > 5:
+        sentence = sentence[-5:]
+
+    cv2.rectangle(image, (0,0), (640, 40), (247, 117, 16), -1)
+    cv2.putText(image, ' '.join(sentence), (3, 30), 
+                   cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+
+    cv2.imshow('ASL', frame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break 
+cap.release
+cv2.destroyAllWindows()
+
+# np.expand_dims(X_test[0], axis=0).shape
+res[np.argmax(res)] > threshold 
+(num_sequences, 30, 1662)
+model.predict(np.expand_dims(X_test[0], axis=0))
